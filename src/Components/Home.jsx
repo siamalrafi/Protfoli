ed5acebc6f88acb91c62161867c2f3141a505951
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-04 16:06:04 +0600

    This is the home

---
+++ src/Components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='grid md:grid-cols-2 sm:grid-cols-1 m-10 '>
+
+                <div className='lg:mt-24 h-96'>
+                    <h1 className="text-2xl ">
+                        Hi! I am
+                    </h1>
+                    <h1 className='my-3 font-bold uppercase text-5xl text-[#2563eb]'>
+                        Md Siam Al Rafi</h1>
+                    <h1 className='my-10 font-bold text-3xl'>
+                        I am A Web Developer.</h1>
+                    <button className="btn btn-primary text-center rounded mx-2">Hire Me</button>
+                    <button className="btn btn-outline text-primary rounded hover:bg-primary ">Resume</button>
+                </div>
+
+                <div className='lg:m-12 flex flex-row-reverse'>
+                    <div className="avatar">
+                        <div className="w-80 mask mask-hexagon">
+                            <img src="https://placeimg.com/192/192/people" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             <a to='/blogs'>Blogs</a></li>
 
         <li className='hover:bg-red-500 font-bold rounded-lg'>
-            <a to='/contact'>Contact</a></li> 
+            <a to='/contact'>Contact</a></li>
 
         <div className="form-control mt-2">
             <label className="label cursor-pointer">
@@ -24,7 +24,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-300">
+            <div className="navbar bg-base-100">
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl font-bold uppercase">Siam</a>
                 </div>
@@ -37,7 +37,7 @@ const Navbar = () => {
                         <button className="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </button>
-                        <ul tabIndex={0} className=" bg-base-300 dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className=" bg-base-100 dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                             {manuItems}
                         </ul>
                     </div>
