74bcc990311879507585163fcb397a487eebb115
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-04 14:09:26 +0600

    This is the protfolio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      <div>
-        {/* <Navbar></Navbar> */}
-        <Header></Header>
+      <div className='bg-base-300'>
+        <Navbar></Navbar>
+        
 
 
+
+
+
+
+        
 
       </div>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
 
-
 const Navbar = () => {
+    const manuItems = <React.Fragment>
 
-    const manuItems =
-        <React.Fragment>
-            <li><a to='/'>Home</a></li>
-            <li><a to='/'>About</a></li>
-            <li><a to='/blogs'>Blogs</a></li>
+        <li className='hover:bg-red-500 font-bold rounded-lg'>
+            <a to='/'>Home</a></li>
 
-            <div className="form-control mt-2">
-                <label className="label cursor-pointer">
-                </label>
-            </div>
-            
-        </React.Fragment>
+        <li className='hover:bg-red-500 font-bold rounded-lg'>
+            <a to='/'>About</a></li>
+
+        <li className='hover:bg-red-500 font-bold rounded-lg'>
+            <a to='/blogs'>Blogs</a></li>
+
+        <li className='hover:bg-red-500 font-bold rounded-lg'>
+            <a to='/contact'>Contact</a></li> 
+
+        <div className="form-control mt-2">
+            <label className="label cursor-pointer">
+            </label>
+        </div>
+    </React.Fragment>
 
 
     return (
         <div>
-            <div className="navbar bg-base-100 flex justify-between">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <div className="navbar bg-base-300">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl font-bold uppercase">Siam</a>
+                </div>
+                <div className="flex-none">
+                    <ul className=" navbar-center hidden lg:flex menu menu-horizontal p-0">
+                        {manuItems}
+                    </ul>
+
+                    <div className="dropdown dropdown-end btn-ghost lg:hidden">
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </button>
+                        <ul tabIndex={0} className=" bg-base-300 dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                             {manuItems}
                         </ul>
                     </div>
-                    <a to='/' className="btn btn-ghost text-xl uppercase">Makbo sell</a>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
-                        {manuItems}
-                    </ul>
-                </div>
-
-                <label htmlFor="deshboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
             </div>
         </div>
     );
