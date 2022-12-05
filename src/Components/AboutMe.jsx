064a1d38df1f2f78878f37a0f7b5c4fc718ffcdc
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-05 20:51:22 +0600

    This is the about section

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -4,21 +4,34 @@ const AboutMe = () => {
     return (
         <div>
             <div className='text-center font-bold text-3xl rounded-box'>
-                <h1>About Me</h1>
+                <h1 className='text-5xl'>About
+                    <span className='ml-3 text-primary'>Me</span>
+                </h1>
             </div>
 
-
-            <div className="hero">
+            <div className="hero m-5">
                 <div className="hero-content flex-col lg:flex-row">
+
                     <div className="avatar">
                         <div className="w-80 mask mask-squircle">
                             <img src="https://placeimg.com/192/192/people" />
                         </div>
                     </div>
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+
+
+                    <div className='lg:px-10  '>
+                        <h1 className='w-full'>Hey! I am <b>Md Siam Al Rafi</b>, a full-stack developer. I am very interested in this sector. I think this will be a growing sector. I'm currently studying at Mymensingh Polytechnic Institute at power engineering.
+                        </h1>
+                        <p>
+                            I am a self-confident, hardworking, and tech-enthusiast person. I always try to learn and explore something new. And I want to use my skills and knowledge in web development.
+                            My biggest strength is that I am a punctual and positive-thinking person. Finally, I want to express my skills and experience with the best company.
+                        </p>
+
+                        <div className='my-5'>
+                            <button className="btn btn-outline text-primary  hover:bg-primary rounded-full">
+                                Download Resume</button>
+                            <button className="btn btn-primary text-center rounded mx-2">GitHub</button>
+                        </div>
                     </div>
                 </div>
             </div>
