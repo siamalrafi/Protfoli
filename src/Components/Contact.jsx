6daf6db968193c4bac91faa517bd1d38c2d80231
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-06 16:11:20 +0600

    This ishte

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,9 +12,42 @@ const Contact = () => {
 
 
 
-            <div>
-                <div></div>
-                <div></div>
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 mx-10'>
+
+                <div className='w-1/3'>
+                    <div>
+                        <h1>Phone</h1>
+                        <p>(+088)01815485598</p>
+                    </div>
+                    <div>
+                        <h1>Email</h1>
+                        <p>rahathasandipto@gmail.com</p>
+                    </div>
+                    <div>
+                        <h1>Location</h1>
+                        <p>Mymensingh, Bangladesh</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 '>
+                        <div  >
+                            <label htmlFor="name" className="">Name</ label>
+                            <br />
+                            <input type="text" placeholder="Type your Name" className="input input-bordered input-md w-full max-w-xs" required />
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="">Email</ label>
+                            <br />
+                            <input type="email" placeholder="Type Your Email" className="input input-bordered input-md w-full max-w-xs" required />
+                        </div>
+                    </div>
+                    <div className='mt-8'>
+                        <label htmlFor="name" className="">Massage</ label>
+                        <textarea className=" textarea w-full textarea-bordered" placeholder="Write your massage"></textarea>
+                    </div>
+                    <button className='btn'>Send Massage</button>
+                </div>
             </div>
         </div>
     );
