3e19a9ab8cabba0c5ea124ad0cd36c1d6e3c6016
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-10 20:04:31 +0600

    dkjfklj

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FaGithub } from 'react-icons/fa';
+import project1 from '../assets/Project/project1.png'
+import project2 from '../assets/Project/project2.png'
+import project3 from '../assets/Project/project3.png'
+import project4 from '../assets/Project/project4.png'
 
 
 
@@ -13,7 +17,7 @@ const Projects = () => {
             name: "Mirraw E-commerce",
             description: "This is a simple e-commerce website . There are several types of second hand products available. You can sell and buy any products if you want to purchase. There are many different Seller who are want to sell there products in this website",
             Technology: ' React,Tailwind,Daisy Ui,Mongodb,Firebase.',
-            img: 'https://source.unsplash.com/random/300x300/?2',
+            img: project1,
             githubUrl: 'https://github.com/siamalrafi/Mirraw-client-website',
             LiveUrl: 'https://mirraw-ebac6.web.app/'
         },
@@ -22,7 +26,7 @@ const Projects = () => {
             name: "Skillshare Academy",
             description: "This project was bootstrapped with Create React App. This is the top class online education platform in the world. In this platform you can learn something new . Many online course are available in here",
             Technology: 'React,Tailwind,Mongodb,Firebase, Private Route,Pdf package',
-            img: 'https://source.unsplash.com/random/300x300/?2',
+            img: project2,
             githubUrl: 'https://github.com/siamalrafi/Skillshare-Academy-projects',
             LiveUrl: 'https://skillshare-academy.web.app/'
         },
@@ -31,7 +35,7 @@ const Projects = () => {
             name: "Dochouse",
             description: 'The DOCHOUSE is a medical service provider that provider various service in reasonable price.This service provider has some extra facilities to handle the customers and get teh best service. The Family Package are very popular in this situation',
             Technology: 'React,Tailwind,Mongodb,Firebase, Private Route',
-            img: 'https://source.unsplash.com/random/300x300/?2',
+            img: project3,
             githubUrl: 'https://github.com/siamalrafi/DOCHOUSE',
             LiveUrl: 'https://dochouse-88db5.web.app/'
         },
@@ -40,7 +44,7 @@ const Projects = () => {
             name: "QuizMaster ",
             description: 'This is a quiz application that you can quiz some interesting topics . There are some most popular and important topics.',
             Technology: 'React,Tailwind,Mongodb,Firebase, Private Route',
-            img: 'https://source.unsplash.com/random/300x300/?2',
+            img: project4,
             githubUrl: 'https://github.com/siamalrafi/QuizMaster-',
             LiveUrl: 'https://idyllic-madeleine-af9e0e.netlify.app/'
         },
@@ -63,7 +67,7 @@ const Projects = () => {
                     projects.map((project, i) =>
                         <div data-aos="fade-up"
                             data-aos-duration="1000" key={i} className="max-w-xs mt-10  rounded-md shadow-2xl hover:shadow-3xl shadow-blue-500/50">
-                            <img src="https://source.unsplash.com/random/300x300/?2" alt=""
+                            <img src={project.img} alt="project.img"
                                 className="object-cover object-center w-full rounded-t-md h-52 dark:bg-gray-500" />
 
                             <div className="flex flex-col justify-between p-6 space-y-8">
